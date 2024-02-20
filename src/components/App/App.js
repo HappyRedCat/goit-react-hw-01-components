023cd43a37895fc0b4dbeb15cac85b032ba9e303
HappyRedCat <HappyRedCat@users.noreply.github.com>
2024-02-20 18:25:38 +0100

    Update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,10 +7,20 @@ import friends from '../../data/friends.json';
 import transactions from '../../data/transactions.json';
 import { Container } from './App.styled';
 import { GlobalStyle } from 'components/GlobalStyle';
-
+import Users from './users.js';
 export const App = () => {
   return (
     <Container>
+      <Users
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
       <Statistics title="Upload stats" data={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
